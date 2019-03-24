9e0254c74e427124c8cc47405ee4f7375922a348
Swarmy0 <jurgen.gonzalezpadilla@outlook.com>
2019-03-23 20:08:18 -0600

    commit test 2

---
+++ Comparar AFDs/src/code/CompararAFDs.java
@@ -0,0 +1,12 @@
+package code;
+
+public class CompararAFDs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

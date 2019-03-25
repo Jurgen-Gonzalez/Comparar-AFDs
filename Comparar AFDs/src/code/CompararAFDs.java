6e862531a0dba8698da454d450994766d03c7e69
Swarmy0 <jurgen.gonzalezpadilla@outlook.com>
2019-03-24 22:18:49 -0600

    Clase Main (utiliza javafx)

--- Comparar AFDs/src/code/CompararAFDs.java
+++ Comparar AFDs/src/code/CompararAFDs.java
@@ -1,12 +1,19 @@
 package code;
 
-public class CompararAFDs {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-    /**
-     * @param args the command line arguments
-     */
+
+
+public class CompararAFDs extends Application{
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        new EntradaDeDatos().launch(primaryStage);
     }
     
 }
